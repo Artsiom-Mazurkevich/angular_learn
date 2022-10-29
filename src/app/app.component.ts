0295c2f99151b56baf426ffd563e10e20c3a1d73
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-10-29 13:56:16 +0300

    learn...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+interface IUser {
+   name: string
+   age: number
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular_lesson';
+   public title: string = 'angular_lesson'
+   user: IUser = {
+      name: 'Yura',
+      age: 20,
+   }
+   isAppLoading = false
+   public disableBtn() {
+      this.isAppLoading = true
+   }
+   text = ''
+
+   changeTextInput(event: Event) {
+      this.text = (event.currentTarget as HTMLInputElement).value
+   }
+
+   two_way_binding_test = 'two_way_binding'
+
+   titleParent = 'titleParent'
 }
